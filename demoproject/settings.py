8b5c4db2294d4e3b3a4ca1df2bc15885e1de5553
nikh1l04 <127930213+nikh1l04@users.noreply.github.com>
2023-04-06 10:48:27 +0530

    Update settings.py

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -77,10 +77,8 @@ WSGI_APPLICATION = 'demoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inmakes_demoproject',
-        'USER':'root',
-        'PASSWORD':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR 'db.sqlite3',
     }
 }
 
